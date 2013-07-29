ce2fb617bfa6ca1cfbbdd3f89411ab5887cf86b7
Alessandro Nax <ale.nassisi@gmail.com>
2013-07-29 09:19:07 +0200

    Defined main app structure (Login - List - CheckIn - DetailsView) Intial graphic

---
+++ src/it/oasis/bussoleno/CardsListFragment.java
@@ -0,0 +1,31 @@
+package it.oasis.bussoleno;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class CardsListFragment extends ListFragment {
+
+	public static CardsListFragment newInstance(){
+		return new CardsListFragment();
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         
+    }
+    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+    	// TODO Auto-generated method stub
+    	super.onActivityCreated(savedInstanceState);
+    	
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+}
--- src/it/oasis/bussoleno/MainActivity.java
+++ src/it/oasis/bussoleno/MainActivity.java
@@ -1,6 +1,8 @@
 package it.oasis.bussoleno;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,29 +10,79 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	private static final String ACTION_LOGIN = "http://www.google.it";
+	private static final String ACTION_GETRESOURCE = "http://www.google.it";
+
+	private static final String TAG_ID = "id";
+	private static final String TAG_DESC = "description";
+	private static final String TAG_NAME = "name";
+	private static final String TAG_DETAILS = "details";
+	private static final String TAG_KIND = "kind";
+	private static final String TAG_VALUE = "value";
+
+	private static final String TAG_FIRST = "first";
+	private static final String TAG_LAST = "last";
+	
+	private static final String CARDS_FRAGMENT = "cards_fragment";
 	Dialog login;
+	Dialog confirm;
+
+	CardsListFragment listFragment = null;
+
 	LoginTask mLoginTask = null;
-    public static final int REQUEST_CODE = 0x0bf7c0de;
-    
+	//public static final int REQUEST_CODE = 0x0bf7c0de;
+	public static final int REQUEST_CODE = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		login = new Dialog(this, android.R.style.Theme_Black_NoTitleBar);
-
+		
+		ArrayList<HashMap<String, String>> tmp = new ArrayList<HashMap<String,String>>();
+		HashMap<String, String> tmp2 = new HashMap<String, String>(); 
+		tmp2.put("first", "antonio");
+		tmp2.put("last", "bianchi");
+		tmp.add(tmp2);
+		tmp2 = new HashMap<String, String>(); 
+		tmp2.put("first", "mario");
+		tmp2.put("last", "rossi");
+		tmp.add(tmp2);
+		tmp2 = new HashMap<String, String>(); 
+		tmp2.put("first", "pinco");
+		tmp2.put("last", "pallo");
+		tmp.add(tmp2);
+		tmp2 = new HashMap<String, String>(); 
+		tmp2.put("first", "antonio");
+		tmp2.put("last", "bianchi");
+		tmp.add(tmp2);
+		if (findViewById(R.id.fragment_container) != null) {
+			ListAdapter adapter = new SimpleAdapter(MainActivity.this,
+					tmp, R.layout.card_item, new String[] { TAG_FIRST, TAG_LAST },
+					new int[] { R.id.first, R.id.last });
+			listFragment = CardsListFragment.newInstance();
+			listFragment.setListAdapter(adapter);
+			getSupportFragmentManager().beginTransaction()
+			.add(R.id.fragment_container, listFragment, CARDS_FRAGMENT).commit();
+		}
+		
+		login = new Dialog(this, android.R.style.Theme_Light_NoTitleBar);
 		login.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		login.setCancelable(false);
 		login.setContentView(R.layout.dialog_login);
@@ -54,8 +106,13 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
 	private void submitLogin(String user, String pwd) {
-		
+
 		mLoginTask = new LoginTask();
 		mLoginTask.execute(user, pwd);
 	}
@@ -68,28 +125,79 @@ public class MainActivity extends Activity {
 	}
 
 	public void scanCode(View v) {
-	    System.out.println("Let's scan!");
-        Intent intentScan = new Intent("com.phonegap.plugins.barcodescanner.SCAN");
-        intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+		System.out.println("Let's scan!");
+		Intent intentScan = new Intent(
+				"com.phonegap.plugins.barcodescanner.SCAN");
+		intentScan.addCategory(Intent.CATEGORY_DEFAULT);
 
-        startActivityForResult(intentScan, REQUEST_CODE);
+		startActivityForResult(intentScan, REQUEST_CODE);
 	}
-	
-	   public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-	        if (requestCode == REQUEST_CODE) {
-	            if (resultCode == Activity.RESULT_OK) {
-	            	System.out.println(intent.getStringExtra("SCAN_RESULT"));
-	            	System.out.println(intent.getStringExtra("SCAN_RESULT_FORMAT"));
-	            }else if (resultCode == Activity.RESULT_CANCELED) {
-	            	System.out.println("RESULT_CANCELED");
-	            } else {
-	            	System.out.println("Error");
-	            }
-	        }
-	    }
 
-	
-	private class LoginTask extends AsyncTask<String, Void, Void>{
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == REQUEST_CODE) {
+			if (resultCode == Activity.RESULT_OK) {
+				System.out.println(intent.getStringExtra("SCAN_RESULT"));
+				System.out.println(intent.getStringExtra("SCAN_RESULT_FORMAT"));
+				new AsyncTask<String, Void, JSONObject>() {
+
+					protected void onPreExecute() {
+						confirm = new Dialog(MainActivity.this,
+								R.style.PauseDialog);
+						confirm.requestWindowFeature(Window.FEATURE_NO_TITLE);
+						confirm.getWindow().setBackgroundDrawable(
+								new ColorDrawable(
+										android.graphics.Color.TRANSPARENT));
+						confirm.setCancelable(true);
+						confirm.setContentView(R.layout.dialog_confirm);
+						WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+						lp.copyFrom(confirm.getWindow().getAttributes());
+						lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+						lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+						confirm.getWindow().setAttributes(lp);
+						confirm.show();
+
+					};
+
+					@Override
+					protected JSONObject doInBackground(String... params) {
+						JSONObject response = null;
+						try {
+							response = ServerUtilities.getJSON(
+									ACTION_GETRESOURCE, "");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						return response;
+					}
+
+					@Override
+					protected void onPostExecute(JSONObject result) {
+						if (true || result != null) {
+							if (confirm != null) {
+								confirm.findViewById(R.id.footer)
+										.setVisibility(View.VISIBLE);
+								confirm.findViewById(R.id.text1).setVisibility(
+										View.VISIBLE);
+								confirm.findViewById(R.id.text2).setVisibility(
+										View.VISIBLE);
+								confirm.findViewById(R.id.text3).setVisibility(
+										View.VISIBLE);
+								confirm.findViewById(R.id.loading)
+										.setVisibility(View.INVISIBLE);
+							}
+						}
+					};
+
+				}.execute("some nasty json");
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				System.out.println("RESULT_CANCELED");
+			} else {
+				System.out.println("Error");
+			}
+		}
+	}
+
+	private class LoginTask extends AsyncTask<String, Void, Void> {
 
 		@Override
 		protected void onPreExecute() {
@@ -103,29 +211,22 @@ public class MainActivity extends Activity {
 			et1.setEnabled(false);
 			et2.setEnabled(false);
 		}
-		
+
 		@Override
 		protected Void doInBackground(String... paramArrayOfParams) {
 			try {
-				ServerUtilities.getJSON("http://www.google.it", "");
+				ServerUtilities.getJSON(ACTION_LOGIN, "");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			return null;
 		}
-		
+
 		protected void onPostExecute(Void result) {
 			// TODO handle response
-			if(login!=null && login.isShowing())
+			if (login != null && login.isShowing())
 				login.dismiss();
 		};
-
-		
 	}
-//	TextView tv = (TextView) findViewById(R.id.textView1);
-//    SimpleDateFormat dfDate_day= new SimpleDateFormat("E, dd/MM/yyyy HH:mm:ss");
-//    String dt="";
-//    Calendar c = Calendar.getInstance(); 
-//    data=dfDate_day.format(c.getTime());
-//    tv.setText(dt);
+
 }

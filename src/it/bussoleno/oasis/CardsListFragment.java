902c3ff0000cd185d98498b7d029c17b08fcce0f
Alessandro N <ale.nassisi@gmail.com>
2014-03-03 18:24:24 +0100

    Initial implementation of an helper view to be showed when lists are empty

--- src/it/bussoleno/oasis/CardsListFragment.java
+++ src/it/bussoleno/oasis/CardsListFragment.java
@@ -3,7 +3,7 @@ package it.bussoleno.oasis;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
-import android.widget.ListView;
+import android.view.ViewGroup;
 
 public class CardsListFragment extends ListFragment {
 
@@ -21,6 +21,9 @@ public class CardsListFragment extends ListFragment {
     	// TODO Auto-generated method stub
     	super.onActivityCreated(savedInstanceState);
     	getListView().setDividerHeight(0);
+    	View emptyView = getActivity().getLayoutInflater().inflate(R.layout.emptylist, null);
+    	getListView().setEmptyView(emptyView);
+        ((ViewGroup) getListView().getParent()).addView(emptyView);
     }
 
     @Override
